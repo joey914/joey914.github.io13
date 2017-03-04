d4351ab985e7feb47b1caf8abfc544e08c2af403
joey914 <joey91426@gmail.com>
2017-03-04 19:30:17 +0900

    fixed..iphone..

--- js/automove.js
+++ js/automove.js
@@ -1,4 +1,18 @@
-
+$("#am1").click(function(){
+  amove(1);
+})
+$("#am2").click(function(){
+  amove(2);
+})
+$("#am3").click(function(){
+  amove(3);
+})
+$("#am4").click(function(){
+  amove(4);
+})
+$("#am_2").click(function(){
+  amove(2);
+})
 function amove(i)
 {
   var ost=$("#div_"+i).offset().top;
