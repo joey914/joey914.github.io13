4b3b213b64d5cb7ca8535bd59b0aafa9c7bb3ad6
joey914 <joey91426@gmail.com>
2017-03-04 16:54:31 +0900

    better function for ios

--- js/automove.js
+++ js/automove.js
@@ -1,5 +1,9 @@
 function amove(i)
 {
   var ost=$("#div_"+i).offset().top;
+  if (navigator.userAgent.match(/(iPod|iPhone|iPad|Android)/)) {
+            window.scrollTo(0,ost-60);
+}
+else
   $('html, body').stop().animate( { scrollTop : ost-60 } );
 }
